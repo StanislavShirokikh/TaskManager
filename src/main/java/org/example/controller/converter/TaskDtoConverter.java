76224b1b6605ae7e235dc5eff5c9878d4b38617d
StanislavShirokikh <shirokikhstanislav@icloud.com>
2023-11-14 00:29:26 +0700

    методы конвертирующие из request в save dto

--- src/main/java/org/example/controller/converter/TaskDtoConverter.java
+++ src/main/java/org/example/controller/converter/TaskDtoConverter.java
@@ -1,7 +1,11 @@
 package org.example.controller.converter;
 
+import org.example.controller.requests.CreateEpicRequest;
+import org.example.controller.requests.CreateSubtaskRequest;
 import org.example.controller.requests.CreateTaskRequest;
 import org.example.controller.requests.UpdateTaskRequest;
+import org.example.dto.SaveEpicDto;
+import org.example.dto.SaveSubTaskDto;
 import org.example.dto.SaveTaskDto;
 import org.example.dto.UpdateTaskDto;
 
@@ -11,6 +15,21 @@ public class TaskDtoConverter {
         saveTaskDto.setName(createTaskRequest.getName());
         saveTaskDto.setDescription(createTaskRequest.getDescription());
         return saveTaskDto;
+    }
+
+    public static SaveEpicDto convert(CreateEpicRequest createEpicRequest) {
+        SaveEpicDto saveEpicDto = new SaveEpicDto();
+        saveEpicDto.setName(createEpicRequest.getName());
+        saveEpicDto.setDescription(createEpicRequest.getDescription());
+        return saveEpicDto;
+    }
+
+    public static SaveSubTaskDto convert(CreateSubtaskRequest createSubtaskRequest) {
+        SaveSubTaskDto saveSubTaskDto = new SaveSubTaskDto();
+        saveSubTaskDto.setName(createSubtaskRequest.getName());
+        saveSubTaskDto.setDescription(createSubtaskRequest.getDescription());
+        saveSubTaskDto.setEpicId(createSubtaskRequest.getEpicId());
+        return saveSubTaskDto;
     }
 
     public static UpdateTaskDto convert(UpdateTaskRequest updateTaskRequest) {
--- src/main/java/org/example/controller/requests/CreateSubtaskRequest.java
+++ src/main/java/org/example/controller/requests/CreateSubtaskRequest.java
@@ -1,6 +1,16 @@
 package org.example.controller.requests;
 
 public class CreateSubtaskRequest extends AbstractCreateTaskRequest{
+    private Integer epicId;
+
+    public Integer getEpicId() {
+        return epicId;
+    }
+
+    public void setEpicId(Integer epicId) {
+        this.epicId = epicId;
+    }
+
     @Override
     public String toString() {
         return "CreateSubtaskRequest{" +
