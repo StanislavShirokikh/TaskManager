efef83b28a97182b5598df0ab8ba81258919fd81
StanislavShirokikh <shirokikhstanislav@icloud.com>
2023-11-16 00:37:40 +0700

    использовал аннотации в сущностях task

--- src/main/java/org/example/dto/AbstractTask.java
+++ src/main/java/org/example/dto/AbstractTask.java
@@ -1,40 +1,13 @@
 package org.example.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public abstract class AbstractTask {
     private String name;
     private String description;
     private Integer id;
     private Status status;
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
 }
