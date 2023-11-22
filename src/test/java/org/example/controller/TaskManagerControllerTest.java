26e7d83286d2466a239933a23e9f2d12b453c188
StanislavShirokikh <shirokikhstanislav@icloud.com>
2023-11-23 02:01:25 +0700

    refactoring

--- src/test/java/org/example/controller/TaskManagerControllerTest.java
+++ src/test/java/org/example/controller/TaskManagerControllerTest.java
@@ -1,6 +1,5 @@
 package org.example.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.controller.requests.CreateEpicRequest;
 import org.example.controller.requests.CreateSubtaskRequest;
@@ -15,13 +14,10 @@ import org.example.dto.SaveTaskDto;
 import org.example.dto.Status;
 import org.example.dto.SubTask;
 import org.example.dto.Task;
-import org.example.dto.UpdateEpicDto;
-import org.example.dto.UpdateTaskDto;
 import org.example.service.manager.Manager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
