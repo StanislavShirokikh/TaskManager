0c6569d6854793ff935d81879c0397daf0e07dc7
StanislavShirokikh <shirokikhstanislav@icloud.com>
2023-12-13 01:13:17 +0700

    refactoring

--- src/main/java/org/example/dao/repository/DataBaseTaskDao.java
+++ src/main/java/org/example/dao/repository/DataBaseTaskDao.java
@@ -1,58 +1,81 @@
 package org.example.dao.repository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.dao.inMemory.TaskDao;
 import org.example.entity.Epic;
-import org.example.entity.Status;
 import org.example.entity.SubTask;
 import org.example.entity.Task;
 import org.example.mappers.EpicRowMapper;
 import org.example.mappers.SubtaskRowMapper;
 import org.example.mappers.TaskMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.Objects;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 @RequiredArgsConstructor
 @ConditionalOnProperty(name = "app.storage.type", havingValue = "DATABASE")
+@Slf4j
 public class DataBaseTaskDao implements TaskDao {
 
     private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert simpleTaskInsert;
+    private final SimpleJdbcInsert simpleEpicInsert;
+    private final SimpleJdbcInsert simpleSubtaskInsert;
+
+    @Autowired
+    public DataBaseTaskDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.simpleTaskInsert = new SimpleJdbcInsert(jdbcTemplate){{
+            withTableName("task");
+            usingGeneratedKeyColumns("id");
+        }};
+        this.simpleEpicInsert = new SimpleJdbcInsert(jdbcTemplate){{
+            withTableName("epic");
+            usingGeneratedKeyColumns("id");
+        }};
+        this.simpleSubtaskInsert = new SimpleJdbcInsert(jdbcTemplate){{
+            withTableName("subtask");
+            usingGeneratedKeyColumns("id");
+        }};
+    }
 
     @Override
     public int saveTask(Task task) {
-        String sql = "INSERT INTO task (name, description, status_id) VALUES(?, ?, ?)";
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(con -> {
-            PreparedStatement ps = con.prepareStatement(sql, new String[] {"id"});
-            ps.setString(1, task.getName());
-            ps.setString(2, task.getDescription());
-            ps.setInt(3, 1);
-            return ps;
-        }, keyHolder);
-        return Objects.requireNonNull(keyHolder.getKey()).intValue();
+        Map<String, Object> map = new HashMap<>(){{
+            put("name", task.getName());
+            put("description", task.getDescription());
+            put("status_id", 1);
+        }};
+
+        return simpleTaskInsert.executeAndReturnKey(map).intValue();
     }
 
     @Override
     public int saveEpic(Epic epic) {
-        String sql = "INSERT INTO epic (name, description) VALUES(?, ?)";
-        jdbcTemplate.update(sql, epic.getName(), epic.getDescription());
-        return 0;
+        Map<String, Object> map = new HashMap<>(){{
+            put("name", epic.getName());
+            put("description", epic.getDescription());
+        }};
+        return simpleEpicInsert.executeAndReturnKey(map).intValue();
     }
 
     @Override
     public int saveSubtask(SubTask subTask) {
-        String sql = "INSERT INTO subtask (name, description, epic_id, status_id) VALUES(?, ?, ?, ?)";
-        jdbcTemplate.update(sql, subTask.getName(), subTask.getDescription(), subTask.getEpicId(), 1);
-        return 0;
+        Map<String, Object> map = new HashMap<>(){{
+            put("name", subTask.getName());
+            put("description", subTask.getDescription());
+            put("status_id", 1);
+            put("epic_id", subTask.getEpicId());
+        }};
+        return simpleSubtaskInsert.executeAndReturnKey(map).intValue();
     }
 
     @Override
@@ -64,9 +87,16 @@ public class DataBaseTaskDao implements TaskDao {
 
     @Override
     public Epic getEpicById(int id) {
-//        String sql = ;
-//        return jdbcTemplate.queryForObject(sql, new EpicRowMapper(), id, id);
-        return null;
+        String sql = "SELECT id, name, description FROM epic WHERE id=?";
+        Epic epic = null;
+        try {
+            epic = jdbcTemplate.queryForObject(sql, new EpicRowMapper(), id);
+            epic.setSubtasksId(getSubtaskIdByEpicId(id));
+
+        } catch (Exception e) {
+            log.info("Epic with id {} not found", id);
+        }
+        return epic;
     }
 
     @Override
@@ -78,16 +108,9 @@ public class DataBaseTaskDao implements TaskDao {
 
     @Override
     public void updateTask(Task task) {
-        int statusId = 0;
-        if (task.getStatus().equals(Status.NEW)) {
-            statusId = 1;
-        } else if (task.getStatus().equals(Status.IN_PROGRESS)) {
-            statusId = 2;
-        }  else if (task.getStatus().equals(Status.DONE)) {
-            statusId = 3;
-        }
-        jdbcTemplate.update("UPDATE task SET name=?, description=?, status_id=? WHERE id=?",
-                task.getName(), task.getDescription(), statusId, task.getId());
+        jdbcTemplate.update("UPDATE task SET name=?, description=?, status_id=(SELECT id FROM status WHERE name=?) " +
+                        "WHERE id=?",
+                task.getName(), task.getDescription(), String.valueOf(task.getStatus()), task.getId());
     }
 
     @Override
@@ -98,16 +121,9 @@ public class DataBaseTaskDao implements TaskDao {
 
     @Override
     public void updateSubTask(SubTask subTask) {
-        int statusId = 0;
-        if (subTask.getStatus().equals(Status.NEW)) {
-            statusId = 1;
-        } else if (subTask.getStatus().equals(Status.IN_PROGRESS)) {
-            statusId = 2;
-        }  else if (subTask.getStatus().equals(Status.DONE)) {
-            statusId = 3;
-        }
-        String sql = "UPDATE subtask SET name=?, description=?, status_id=?, epic_id=? WHERE id=? ";
-        jdbcTemplate.update(sql, subTask.getName(), subTask.getDescription(), statusId, subTask.getEpicId(),
+        String sql = "UPDATE subtask SET name=?, description=?, status_id=(SELECT id FROM status WHERE name=?)," +
+                " epic_id=? WHERE id=? ";
+        jdbcTemplate.update(sql, subTask.getName(), subTask.getDescription(), subTask.getStatus(), subTask.getEpicId(),
                 subTask.getId());
     }
 
@@ -118,7 +134,7 @@ public class DataBaseTaskDao implements TaskDao {
 
     @Override
     public void removeEpicById(int id) {
-        String sql1 = "DELETE FROM subtask WHERE epic_id=?";
+        String sql1 = "DELETE FROM epic WHERE id=?";
         jdbcTemplate.update(sql1, id);
     }
 
@@ -126,5 +142,9 @@ public class DataBaseTaskDao implements TaskDao {
     public void removeSubTaskById(int id) {
         String sql = "DELETE FROM subtask WHERE id=?";
         jdbcTemplate.update(sql, id);
+    }
+    private List<Integer> getSubtaskIdByEpicId(int epicId) {
+        String sql = "SELECT id FROM subtask WHERE epic_id=?";
+        return jdbcTemplate.queryForList(sql, Integer.class, epicId);
     }
 }
--- src/main/java/org/example/mappers/EpicRowMapper.java
+++ src/main/java/org/example/mappers/EpicRowMapper.java
@@ -1,15 +1,10 @@
 package org.example.mappers;
 
 import org.example.entity.Epic;
-import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class EpicRowMapper implements RowMapper<Epic> {
     @Override
